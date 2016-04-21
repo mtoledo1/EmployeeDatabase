cb9de9dbd21242daff7ea14d931a5e669b75c2a2
mat_688@yahoo.ca <uncharted3>
2016-04-21 14:34:39 -0500

    The Code Works.

--- app/src/main/java/commrhardman23/httpsgithub/employeedatabase/EmployeeDatabaseInterface.java
+++ app/src/main/java/commrhardman23/httpsgithub/employeedatabase/EmployeeDatabaseInterface.java
@@ -1,11 +1,19 @@
 package commrhardman23.httpsgithub.employeedatabase;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class EmployeeDatabaseInterface extends AppCompatActivity {
 
@@ -14,6 +22,11 @@ public class EmployeeDatabaseInterface extends AppCompatActivity {
     private EditText edtxtEmployeeNum;
     private EditText edtxtWage;
     private TextView txtvwResult;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +39,17 @@ public class EmployeeDatabaseInterface extends AppCompatActivity {
         edtxtWage = (EditText) findViewById(R.id.edtxtWage);
         txtvwResult = (TextView) findViewById(R.id.txtvwResult);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
      * insertData adds elements to the Employee database using information given by the user
+     *
      * @param vw is the button the method is associated with
      */
-    private void insertData(View vw){
+    public void insertData(View vw) {
 
         /**
          * 1. Create a new EmployeeDatabaseHelper variable. You will need to use the following call:
@@ -42,23 +59,46 @@ public class EmployeeDatabaseInterface extends AppCompatActivity {
          * 4. Get a Writable Database reference using the variable name db (Remember your
          *    try-catch block. The if-else statement that follows should also go in your try block).
          */
+        EmployeeDatabaseHelper employeeDatabaseHelper = new EmployeeDatabaseHelper(this, null, null, 0);
+        SQLiteDatabase db;
+        String name;
+        String position;
+        String EmployeeNum;
+        String wage;
+        ContentValues contentVal = new ContentValues();
+        try {
+            db = employeeDatabaseHelper.getWritableDatabase();
+            //insert code from if-else statement here
+            if (edtxtName.getText().length() == 0 || edtxtPosition.getText().length() == 0 ||
+                    edtxtEmployeeNum.getText().length() == 0 || edtxtWage.getText().length() == 0) {
 
-        if(edtxtName.getText().length() == 0 || edtxtPosition.getText().length() == 0 ||
-                edtxtEmployeeNum.getText().length() == 0 || edtxtWage.getText().length() == 0){
+                txtvwResult.setText("You must enter all values to add an element!");
 
-            txtvwResult.setText("You must enter all values to add an element!");
+            } else {
+                /**
+                 * 1. Set each variable equal to the values from the EditTexts
+                 * 2. put each value into the ContentValues variable
+                 * 3. Call the EmployeeDatabaseHelper's insertElement method
+                 * 4. Display that the element has been added successfully
+                 */
+                name = edtxtName.getText().toString();
+                position = edtxtPosition.getText().toString();
+                EmployeeNum = edtxtEmployeeNum.getText().toString();
+                wage = edtxtWage.getText().toString();
+                contentVal.put("NAME", name);
+                contentVal.put("POSITION", position);
+                contentVal.put("EMPLOYEE_NUM", EmployeeNum);
+                contentVal.put("WAGE", wage);
 
-        } else {
+                employeeDatabaseHelper.insertElement(db, contentVal);
 
-            /**
-             * 1. Set each variable equal to the values from the EditTexts
-             * 2. put each value into the ContentValues variable
-             * 3. Call the EmployeeDatabaseHelper's insertElement method
-             * 4. Display that the element has been added successfully
-             */
+                txtvwResult.setText("Information Has Been Successfully Added");
 
+            }
+        } catch (SQLiteException e) {
+            //display that the database was not found
+            txtvwResult.setText("Information not found. :(");
         }
-
     }
 
     /**
@@ -66,7 +106,7 @@ public class EmployeeDatabaseInterface extends AppCompatActivity {
      * in the Employee database
      * @param vw is the button that is associated with this method
      */
-    private void searchOrDelete(View vw){
+    public void searchOrDelete(View vw) {
 
         Intent goToSearchDelete = new Intent(this, SearchDatabase.class);
 
@@ -82,5 +122,45 @@ public class EmployeeDatabaseInterface extends AppCompatActivity {
         edtxtWage.setText("");
         txtvwResult.setText("");
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "EmployeeDatabaseInterface Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://commrhardman23.httpsgithub.employeedatabase/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "EmployeeDatabaseInterface Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://commrhardman23.httpsgithub.employeedatabase/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
--- app/src/main/java/commrhardman23/httpsgithub/employeedatabase/SearchDatabase.java
+++ app/src/main/java/commrhardman23/httpsgithub/employeedatabase/SearchDatabase.java
@@ -2,11 +2,19 @@ package commrhardman23.httpsgithub.employeedatabase;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.sql.SQLException;
 
 public class SearchDatabase extends AppCompatActivity {
 
@@ -15,6 +23,11 @@ public class SearchDatabase extends AppCompatActivity {
     EditText edtxtEmployeeNum;
     EditText edtxtWage;
     TextView txtvwResult;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +40,18 @@ public class SearchDatabase extends AppCompatActivity {
         edtxtWage = (EditText) findViewById(R.id.edtxtWage);
         txtvwResult = (TextView) findViewById(R.id.txtvwResult);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
      * searchDatabase gets all the information the user wants to search for, searches the database
      * for it, and displays the result in a TextView.
+     *
      * @param vw is the button this method is associated with
      */
-    private void searchDatabase(View vw){
+    public void searchDatabase(View vw) {
 
         int numOfArguments = 0;
         int indexOfSearchArray = 0;
@@ -45,11 +62,11 @@ public class SearchDatabase extends AppCompatActivity {
         String whereToSearch = "";
         String[] elementsToSearch;
         EmployeeDatabaseHelper employeeDatabaseHelper = new EmployeeDatabaseHelper(this, null, null, 0);
-        SQLiteDatabase db;
+        SQLiteDatabase db = null;
         Cursor searchCursor;
 
 
-        if(edtxtName.getText().length() != 0){
+        if (edtxtName.getText().length() != 0) {
 
             numOfArguments++;
             name = edtxtName.getText().toString();
@@ -57,12 +74,12 @@ public class SearchDatabase extends AppCompatActivity {
             whereToSearch += "NAME = ?";
         }
 
-        if(edtxtPosition.getText().length() != 0){
+        if (edtxtPosition.getText().length() != 0) {
 
             numOfArguments++;
             position = edtxtPosition.getText().toString();
 
-            if(whereToSearch.equals("")) {
+            if (whereToSearch.equals("")) {
                 whereToSearch += "POSITION = ?";
             } else {
                 whereToSearch += " OR POSITION = ?";
@@ -71,12 +88,12 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(edtxtEmployeeNum.getText().length() != 0){
+        if (edtxtEmployeeNum.getText().length() != 0) {
 
             numOfArguments++;
             employeeNum = edtxtEmployeeNum.getText().toString();
 
-            if(whereToSearch.equals("")) {
+            if (whereToSearch.equals("")) {
                 whereToSearch += "EMPLOYEE_NUM = ?";
             } else {
                 whereToSearch += " OR EMPLOYEE_NUM = ?";
@@ -85,12 +102,12 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(edtxtWage.getText().length() != 0){
+        if (edtxtWage.getText().length() != 0) {
 
             numOfArguments++;
             wage = edtxtWage.getText().toString();
 
-            if(whereToSearch.equals("")) {
+            if (whereToSearch.equals("")) {
                 whereToSearch += "WAGE = ?";
             } else {
                 whereToSearch += " OR WAGE = ?";
@@ -99,7 +116,7 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(numOfArguments == 0){
+        if (numOfArguments == 0) {
 
             elementsToSearch = null;
             whereToSearch = null;
@@ -108,28 +125,28 @@ public class SearchDatabase extends AppCompatActivity {
 
             elementsToSearch = new String[numOfArguments];
 
-            if(edtxtName.getText().length() != 0){
+            if (edtxtName.getText().length() != 0) {
 
                 elementsToSearch[indexOfSearchArray] = name;
                 indexOfSearchArray++;
 
             }
 
-            if(edtxtPosition.getText().length() != 0){
+            if (edtxtPosition.getText().length() != 0) {
 
                 elementsToSearch[indexOfSearchArray] = position;
                 indexOfSearchArray++;
 
             }
 
-            if(edtxtEmployeeNum.getText().length() != 0){
+            if (edtxtEmployeeNum.getText().length() != 0) {
 
                 elementsToSearch[indexOfSearchArray] = employeeNum;
                 indexOfSearchArray++;
 
             }
 
-            if(edtxtWage.getText().length() != 0){
+            if (edtxtWage.getText().length() != 0) {
 
                 elementsToSearch[indexOfSearchArray] = wage;
                 indexOfSearchArray++;
@@ -159,27 +176,48 @@ public class SearchDatabase extends AppCompatActivity {
          *
          *              txtvwResult.setText(txtvwResult.getText().toString() +
          *                  String.format("Name: %-20s Position %-20s\nEmployee Number: %-20d" +
-         *                                  " Wage: -20.2f\n", searchCursor.getString(0),
-         *                                  searchCursor.getString(1), searchCursor.getInt(2),
-         *                                  searchCursor.getDouble(3)));
+         *                                  " Wage: -20.2f\n", searchCursor.getString(1),
+         *                                  searchCursor.getString(2), searchCursor.getInt(3),
+         *                                  searchCursor.getDouble(4)));
          *
          *              How do we get the next row in the Cursor? Put that here...
-         *
          *          }
-         *
          *      }
          * }
          *
          */
+        try {
+            db = employeeDatabaseHelper.getReadableDatabase();
+            searchCursor = db.query("EMPLOYEE", new String[]{"NAME, POSITION, EMPLOYEE_NUM, WAGE"},
+                    whereToSearch, elementsToSearch, null, null, null);
+            if (searchCursor.getCount() == 0) {
+                txtvwResult.setText("There are no entries with this info...");
+            } else {
+                if (searchCursor.moveToFirst()) {
+                    for (int i = 0; i < searchCursor.getCount(); i++) {
+                        txtvwResult.setText(txtvwResult.getText().toString() +
+                                String.format("Name: %-35s Position: %s\nEmployee Number: %-15d" +
+                                                " Wage: $%.2f\n", searchCursor.getString(0),
+                                        searchCursor.getString(1), searchCursor.getInt(2),
+                                        searchCursor.getDouble(3)));
+
+                        //How do we get the next row in the Cursor? Put that here...
+                    }
+                }
+            }
+        } catch (SQLiteException e) {
+            txtvwResult.setText("Database not Found");
+        }
 
     }
 
     /**
      * deleteEntry gets all the deletion criteria from the user and deletes all entries in the
      * database that contain the information provided
+     *
      * @param vw is the button the method is associated with
      */
-    private void deleteEntry(View vw){
+    public void deleteEntry(View vw) {
 
         int numOfArguments = 0;
         int indexOfSearchArray = 0;
@@ -193,7 +231,7 @@ public class SearchDatabase extends AppCompatActivity {
         SQLiteDatabase db;
         int numRowsDeleted;
 
-        if(edtxtName.getText().length() != 0){
+        if (edtxtName.getText().length() != 0) {
 
             numOfArguments++;
             name = edtxtName.getText().toString();
@@ -201,12 +239,12 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(edtxtPosition.getText().length() != 0){
+        if (edtxtPosition.getText().length() != 0) {
 
             numOfArguments++;
             position = edtxtPosition.getText().toString();
 
-            if(whereToDelete.equals("")) {
+            if (whereToDelete.equals("")) {
                 whereToDelete += "POSITION = ?";
             } else {
                 whereToDelete += " AND POSITION = ?";
@@ -214,12 +252,12 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(edtxtEmployeeNum.getText().length() != 0){
+        if (edtxtEmployeeNum.getText().length() != 0) {
 
             numOfArguments++;
             employeeNum = edtxtEmployeeNum.getText().toString();
 
-            if(whereToDelete.equals("")) {
+            if (whereToDelete.equals("")) {
                 whereToDelete += "EMPLOYEE_NUM = ?";
             } else {
                 whereToDelete += " AND EMPLOYEE_NUM = ?";
@@ -227,12 +265,12 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(edtxtWage.getText().length() != 0){
+        if (edtxtWage.getText().length() != 0) {
 
             numOfArguments++;
             wage = edtxtWage.getText().toString();
 
-            if(whereToDelete.equals("")) {
+            if (whereToDelete.equals("")) {
                 whereToDelete += "WAGE = ?";
             } else {
                 whereToDelete += " AND WAGE = ?";
@@ -240,7 +278,7 @@ public class SearchDatabase extends AppCompatActivity {
 
         }
 
-        if(numOfArguments == 0){
+        if (numOfArguments == 0) {
 
             elementsToDelete = null;
             whereToDelete = null;
@@ -287,7 +325,52 @@ public class SearchDatabase extends AppCompatActivity {
          *    the String where and String[] whereArgs parameters, respectively
          * 3. Display the number of rows deleted
          */
+        db = employeeDatabaseHelper.getWritableDatabase();
+        try {
+            numRowsDeleted = employeeDatabaseHelper.deleteElement(db, whereToDelete, elementsToDelete);
 
+        } catch (SQLiteException e) {
+
+        }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SearchDatabase Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://commrhardman23.httpsgithub.employeedatabase/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "SearchDatabase Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://commrhardman23.httpsgithub.employeedatabase/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
